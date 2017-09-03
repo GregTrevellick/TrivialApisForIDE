993b8cc72cb2a8a8d99f28fd277211bf9c71ca13
Greg Trevellick <gtrevellick@googlemail.com>
2017-09-03 17:54:18 +0100

    .

--- src/Trivial.Ui.TrumpQuotes/Properties/AssemblyInfo.cs
+++ src/Trivial.Ui.TrumpQuotes/Properties/AssemblyInfo.cs
@@ -1,18 +1,19 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Trivial.Ui.TrumpQuotes;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Trivial.Ui.TrumpQuotes")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle(Vsix.Name)]
+[assembly: AssemblyDescription(Vsix.Description)]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Trivial.Ui.TrumpQuotes")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany(Vsix.Author)]
+[assembly: AssemblyProduct(Vsix.Name)]
+[assembly: AssemblyCopyright(Vsix.Author)]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture(Vsix.Language)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -31,3 +32,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyVersion(Vsix.Version + ".*")] gregt
---
+++ src/Trivial.Ui.TrumpQuotes/VSPackage.cs
@@ -0,0 +1,48 @@
+﻿using EnvDTE;
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+using Trivial.Entities;
+using Trivial.Ui.Common;
+
+namespace Trivial.Ui.TrumpQuotes
+{
+    [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration(productName: "#110", productDetails: "#112", productId: Vsix.Version, IconResourceID = 400)]
+    [Guid(Vsix.Id)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    //TODO GREGT [ProvideOptionPage(typeof(OptionsDialogPage), Vsix.Name, "General", 0, 0, supportsAutomation: true)]
+    public sealed class VSPackage : Package
+    {
+        private DTE dte;
+
+        public VSPackage()
+        {
+            //gregt call the web service here for performance sake ?
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            IServiceContainer serviceContainer = this as IServiceContainer;
+            dte = serviceContainer.GetService(typeof(SDTE)) as DTE;
+            var solutionEvents = dte.Events.SolutionEvents;
+            solutionEvents.Opened += OnSolutionOpened;
+        }
+
+        private void OnSolutionOpened()
+        {
+            TriviaHelper.ShowTrivia(AppName.TrumpQuotes, Vsix.Name);
+        }
+    }
+}
