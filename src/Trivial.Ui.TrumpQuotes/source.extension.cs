d24a884f1d3657d57db2428f96b2d2b06be7f775
Greg Trevellick <gtrevellick@googlemail.com>
2017-09-04 07:28:48 +0100

    .

--- src/Trivial.Ui.TrumpQuotes/source.extension.cs
+++ src/Trivial.Ui.TrumpQuotes/source.extension.cs
@@ -11,7 +11,7 @@ namespace Trivial.Ui.TrumpQuotes
         public const string Name = "Drain The Swamp";
         public const string Description = @"A random Donald Trump quote in a pop-up whenever you open a solution.";
         public const string Language = "en-US";
-        public const string Version = "1.0.41";
+        public const string Version = "1.0.42";
         public const string Author = "Greg Trevellick";
         public const string Tags = "tronalddump,  tronald, dump, donald trump, donald, trump, potus, drain, swamp, drain the swamp";
     }
