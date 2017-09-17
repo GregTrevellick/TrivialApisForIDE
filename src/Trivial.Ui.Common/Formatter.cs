da221f8102fa82e063ab8d5929a53aca85b6d990
Greg Trevellick <gtrevellick@googlemail.com>
2017-09-17 08:03:32 +0100

    .

--- src/Trivial.Ui.Common/Formatter.cs
+++ src/Trivial.Ui.Common/Formatter.cs
@@ -8,13 +8,7 @@ namespace Trivial.Ui.Common
         public static string GetBody(string text, string attribution)
         {
             var popUpBody = text + Environment.NewLine + Environment.NewLine +
-                            attribution + Environment.NewLine + Environment.NewLine;
-
-            //var bold = new Bold(new Run("Bold Text"));
-            //tb.Inlines.Add(bold);
-
-            //var normal = new Run("Normal Text"));
-            //tb.Inlines.Add(normal);
+                            attribution;
 
             return popUpBody;
         }
