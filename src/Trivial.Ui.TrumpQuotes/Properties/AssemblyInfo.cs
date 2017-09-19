cb87ad95de3715387158097f237ba1eb72e35834
Greg Trevellick <gtrevellick@googlemail.com>
2017-09-19 21:16:15 +0100

    .

--- src/Trivial.Ui.TrumpQuotes/Properties/AssemblyInfo.cs
+++ src/Trivial.Ui.TrumpQuotes/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using Trivial.Ui.TrumpQuotes;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-//[assembly: AssemblyVersion(Vsix.Version + ".*")] gregt
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(Vsix.Version + ".*")] 
