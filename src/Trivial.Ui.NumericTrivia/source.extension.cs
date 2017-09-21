cd6d7146113a36e180f2acf7ded3b927033edf2f
Greg Trevellick <gtrevellick@googlemail.com>
2017-09-21 10:23:21 +0100

    .

--- src/Trivial.Ui.NumericTrivia/source.extension.cs
+++ src/Trivial.Ui.NumericTrivia/source.extension.cs
@@ -9,9 +9,9 @@ namespace Trivial.Ui.NumericTrivia
     {
         public const string Id = "f39b9a5e-80dc-4b2c-9cc1-c015d8ff9840";
         public const string Name = "Numeric Trivia";
-        public const string Description = @"A random numeral-based trivia fact in a pop-up whenever you open a solution.";
+        public const string Description = @"A random pop-up numeric fact whenever you open a solution.";
         public const string Language = "en-US";
-        public const string Version = "1.0.52";
+        public const string Version = "1.0.53";
         public const string Author = "Greg Trevellick";
         public const string Tags = "numbers, trivia, trivial, meaningless, fun, numeral, math";
     }
