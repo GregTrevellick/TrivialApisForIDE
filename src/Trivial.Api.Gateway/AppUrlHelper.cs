8a7c0ab820e16a86671914e7d6653ae0b8b8a9e7
Greg Trevellick <gtrevellick@googlemail.com>
2017-09-22 23:22:46 +0100

    .

--- src/Trivial.Api.Gateway/AppUrlHelper.cs
+++ src/Trivial.Api.Gateway/AppUrlHelper.cs
@@ -12,19 +12,11 @@ namespace Trivial.Api.Gateway
             {
                 case AppName.NumericTrivia:
                     //Consistent average response time 330ms
-                    url =  //"http://numbersapi.com/random/trivia";
-
-
-                  // YES   "http://api.fixer.io/latest"
-                  // YES   "http://www.jservice.io/api/random"
-
-
-
-
-                        ;
-
-
-
+                    url = "http://numbersapi.com/random/trivia";
+                        // YES "http://api.fixer.io/latest"
+                        // YES "http://www.jservice.io/api/random"
+                        // YES "https://opentdb.com/api.php?amount=1&category=18&difficulty=easy&type=multiple" //https://opentdb.com/api_config.php 
+                        // YES "https://qriusity.com/v1/questions?page=2&limit=1"
                     break;
                 case AppName.TrumpQuotes:
                     //Average response time 280ms but sometimes peaking around 370ms
@@ -37,17 +29,11 @@ namespace Trivial.Api.Gateway
     }
 }
 
-
 //"https://icanhazdadjoke.com/api#fetch-a-random-dad-joke"
 ////"https://quotesondesign.com/api-v4-0/"
 //"https://restcountries.eu/"
-//PAID     "http://quotes.rest/#!/quote/get_quote_random"
-
-
-
+//PAID "http://quotes.rest/#!/quote/get_quote_random"
 ////https://www.coindesk.com/api/ 
-////https://opentdb.com/api_config.php 
-////https://qriusity.com/ 
 ////https://api.pandascore.co/rest#make-basic-requests 
 ////https://data.police.uk/docs/ 
 ////https://www.meetup.com/meetup_api/ 
