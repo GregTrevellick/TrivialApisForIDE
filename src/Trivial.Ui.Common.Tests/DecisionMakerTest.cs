a5174b4e0d353a8af4e56b4b91d7a938fcb7ea7c
Greg Trevellick <gtrevellick@googlemail.com>
2017-09-24 19:25:26 +0100

    .

--- src/Trivial.Ui.Common.Tests/DecisionMakerTest.cs
+++ src/Trivial.Ui.Common.Tests/DecisionMakerTest.cs
@@ -4,36 +4,37 @@ using NUnit.Framework;
 namespace Trivial.Ui.Common.Tests
 {
     [TestFixture]
-    public class TriviaHelperTest
+    public class DecisionMakerTest
     {
         [Test]
         [Category("U")]
         public void LastPopUpMoreThanXMinutesAgoTest()
         {
             //Arrange
-            var triviaHelper = new DecisionMaker();
+            var decisionMaker = new DecisionMaker();
+
             //Act
-            Assert.IsFalse(triviaHelper.LastPopUpMoreThanXMinutesAgo(
+            Assert.IsFalse(decisionMaker.LastPopUpMoreThanXMinutesAgo(
                 lastPopUpDateTime: new DateTime(2017, 01, 01, 13, 0, 0), 
                 popUpIntervalInMins: 60,
                 dateTimeNow:  new DateTime(2017, 01, 01, 13, 0, 0)));
 
-            Assert.IsTrue(triviaHelper.LastPopUpMoreThanXMinutesAgo(
+            Assert.IsTrue(decisionMaker.LastPopUpMoreThanXMinutesAgo(
                 lastPopUpDateTime: new DateTime(2017, 01, 01, 12, 00, 00),
                 popUpIntervalInMins: 60,
                 dateTimeNow: new DateTime(2017, 01, 01, 13, 0, 0)));
 
-            Assert.IsTrue(triviaHelper.LastPopUpMoreThanXMinutesAgo(
+            Assert.IsTrue(decisionMaker.LastPopUpMoreThanXMinutesAgo(
                 lastPopUpDateTime: new DateTime(2017, 01, 01, 11, 59, 59),
                 popUpIntervalInMins: 60,
                 dateTimeNow: new DateTime(2017, 01, 01, 13, 0, 0)));
 
-            Assert.IsTrue(triviaHelper.LastPopUpMoreThanXMinutesAgo(
+            Assert.IsTrue(decisionMaker.LastPopUpMoreThanXMinutesAgo(
                 lastPopUpDateTime: new DateTime(2017, 01, 01, 13, 00, 00),
                 popUpIntervalInMins: 0,
                 dateTimeNow: new DateTime(2017, 01, 01, 13, 0, 0)));
 
-            Assert.IsTrue(triviaHelper.LastPopUpMoreThanXMinutesAgo(
+            Assert.IsTrue(decisionMaker.LastPopUpMoreThanXMinutesAgo(
                 lastPopUpDateTime: new DateTime(2017, 01, 01, 12, 59, 59),
                 popUpIntervalInMins: 0,
                 dateTimeNow: new DateTime(2017, 01, 01, 13, 0, 0)));
