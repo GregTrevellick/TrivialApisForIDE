1abe4bfeb5a02f71ce62a4d573c2dfc4f932e87c
Greg Trevellick <gtrevellick@googlemail.com>
2017-10-16 08:08:51 +0100

    .

--- src/Trivial.Ui.Common/zTriviaDialog.xaml.cs
+++ src/Trivial.Ui.Common/zTriviaDialog.xaml.cs
@@ -1,251 +1,251 @@
-﻿//using System;
-//using Microsoft.VisualStudio.PlatformUI;
-//using System.Diagnostics;
-//using System.Windows;
-//using System.Windows.Media;
-//using System.Windows.Media.Imaging;
-//using System.Windows.Navigation;
-//using Trivial.Api.Gateway;
-//using Trivial.Entities;
+﻿using System;
+using Microsoft.VisualStudio.PlatformUI;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Trivial.Api.Gateway;
+using Trivial.Entities;
 
-//namespace Trivial.Ui.Common
-//{
-//    public partial class zTriviaDialog : DialogWindow
-//    {
-//        private AppName _appName;
-//        private string _correctAnswer;
-//        private string _optionsName;
-//        public QuestionType _questionType;
-//        public bool _suppressClosingWithoutSubmitingAnswerWarning;
-//        public int? _totalQuestionsAnsweredCorrectly;
-//        public int? _totalQuestionsAsked;
-//        private bool _userStatusTotalsIncremented;
-//        public delegate void MyEventHandler(int? totalQuestionsAsked, int? totalQuestionsAnsweredCorrectly);
-//        public event MyEventHandler PersistHiddenOptionsEventHandler;
+namespace Trivial.Ui.Common
+{
+    public partial class zTriviaDialog : DialogWindow
+    {
+        private AppName _appName;
+        private string _correctAnswer;
+        private string _optionsName;
+        public QuestionType _questionType;
+        public bool _suppressClosingWithoutSubmitingAnswerWarning;
+        public int? _totalQuestionsAnsweredCorrectly;
+        public int? _totalQuestionsAsked;
+        private bool _userStatusTotalsIncremented;
+        public delegate void MyEventHandler(int? totalQuestionsAsked, int? totalQuestionsAnsweredCorrectly);
+        public event MyEventHandler PersistHiddenOptionsEventHandler;
 
-//        public zTriviaDialog(AppName appName, string optionsName)
-//        {
-//            _appName = appName;
-//            _optionsName = optionsName;
-//            _userStatusTotalsIncremented = false;
+        public zTriviaDialog(AppName appName, string optionsName)
+        {
+            _appName = appName;
+            _optionsName = optionsName;
+            _userStatusTotalsIncremented = false;
 
-//            InitializeComponent();
-//            InitializeTriviaDialog();
+            InitializeComponent();
+            InitializeTriviaDialog();
 
-//            DataContext = this;
+            DataContext = this;
 
-//            switch (_appName)
-//            {
-//                case AppName.GeekQuiz:
-//                    StackPanelGeekQuiz.Visibility = Visibility.Visible;
-//                    break;
-//                case AppName.Jeopardy:
-//                    StackPanelJeopardy.Visibility = Visibility.Visible;
-//                    break;
-//                case AppName.NumericTrivia:
-//                    StackPanelNumericTrivia.Visibility = Visibility.Visible;
-//                    break;
-//                case AppName.TrumpQuotes:
-//                    StackPanelTrumpQuotes.Visibility = Visibility.Visible;
-//                    break;
-//            }
-//        }
+            switch (_appName)
+            {
+                case AppName.GeekQuiz:
+                    StackPanelGeekQuiz.Visibility = Visibility.Visible;
+                    break;
+                case AppName.Jeopardy:
+                    StackPanelJeopardy.Visibility = Visibility.Visible;
+                    break;
+                case AppName.NumericTrivia:
+                    StackPanelNumericTrivia.Visibility = Visibility.Visible;
+                    break;
+                case AppName.TrumpQuotes:
+                    StackPanelTrumpQuotes.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
 
-//        private void InitializeTriviaDialog()
-//        {
-//            HasMaximizeButton = true;
-//            HasMinimizeButton = true;
-//            SizeToContent = SizeToContent.WidthAndHeight;
-//            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        private void InitializeTriviaDialog()
+        {
+            HasMaximizeButton = true;
+            HasMinimizeButton = true;
+            SizeToContent = SizeToContent.WidthAndHeight;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
 
-//            //var iconUri = new TriviaMessage().GetIconUri(_appName);
-//            //Icon = new BitmapImage(iconUri);
-//        }
+            //var iconUri = new TriviaMessage().GetIconUri(_appName);
+            //Icon = new BitmapImage(iconUri);
+        }
 
-//        private void AppBtnHelp_OnClick(object sender, RoutedEventArgs e)
-//        {
-//            if (TextBlockHelp.Visibility == Visibility.Visible)
-//            {
-//                TextBlockHelp.Visibility = Visibility.Collapsed;
-//            }
-//            else
-//            {
-//                TextBlockHelp.Text = $"To alter frequency and volume of delivery go to Tools | Options | {_optionsName}";
-//                TextBlockHelp.Visibility = Visibility.Visible;
-//            }
-//        }
+        private void AppBtnHelp_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (TextBlockHelp.Visibility == Visibility.Visible)
+            {
+                TextBlockHelp.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                TextBlockHelp.Text = $"To alter frequency and volume of delivery go to Tools | Options | {_optionsName}";
+                TextBlockHelp.Visibility = Visibility.Visible;
+            }
+        }
 
-//        private void AppBtnClose_OnClick(object sender, RoutedEventArgs e)
-//        {
-//            var shouldClose = true;
+        private void AppBtnClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            var shouldClose = true;
 
-//            if (_questionType != QuestionType.None)
-//            {
-//                if (AppBtnGeekQuizSubmitMultiChoiceAnwser.IsEnabled)
-//                {
-//                    if (!_suppressClosingWithoutSubmitingAnswerWarning)
-//                    {
-//                        var closeWithoutSubmitingAnswer = MessageBoxes.ConfirmCloseWithoutSubmitingAnswer(_optionsName);
+            if (_questionType != QuestionType.None)
+            {
+                if (AppBtnGeekQuizSubmitMultiChoiceAnwser.IsEnabled)
+                {
+                    if (!_suppressClosingWithoutSubmitingAnswerWarning)
+                    {
+                        var closeWithoutSubmitingAnswer = MessageBoxes.ConfirmCloseWithoutSubmitingAnswer(_optionsName);
 
-//                        if (!closeWithoutSubmitingAnswer)
-//                        {
-//                            shouldClose = false;
-//                        }
-//                    }
-//                }
-//            }
+                        if (!closeWithoutSubmitingAnswer)
+                        {
+                            shouldClose = false;
+                        }
+                    }
+                }
+            }
 
-//            if (shouldClose)
-//            {
-//                Close();
-//            }
-//        }
+            if (shouldClose)
+            {
+                Close();
+            }
+        }
 
-//        #region not shared
-//        private void AppHyperlink1_RequestNavigate(object sender, RequestNavigateEventArgs e)//TrumpQuotes
-//        {
-//            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
-//            e.Handled = true;
-//        }
+        #region not shared
+        private void AppHyperlink1_RequestNavigate(object sender, RequestNavigateEventArgs e)//TrumpQuotes
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
 
-//        private void AppBtnRevealAnswer_OnClick(object sender, RoutedEventArgs e)//Jeopardy
-//        {
-//            AppBtnRevealAnswerJeopardy.Visibility = Visibility.Collapsed;
-//            AppTextBlockAnswerJeopardy.Visibility = Visibility.Visible;
-//        }
+        private void AppBtnRevealAnswer_OnClick(object sender, RoutedEventArgs e)//Jeopardy
+        {
+            AppBtnRevealAnswerJeopardy.Visibility = Visibility.Collapsed;
+            AppTextBlockAnswerJeopardy.Visibility = Visibility.Visible;
+        }
 
-//        private void AppBtnSubmitMultiChoiceAnwser_OnClick(object sender, RoutedEventArgs e)//GeekQuiz
-//        {
-//            string response;
+        private void AppBtnSubmitMultiChoiceAnwser_OnClick(object sender, RoutedEventArgs e)//GeekQuiz
+        {
+            string response;
 
-//            if (RadioBtn1.IsChecked == true)
-//            {
-//                response = RadioBtn1.Content.ToString();
-//            }
-//            else
-//            {
-//                if (RadioBtn2.IsChecked == true)
-//                {
-//                    response = RadioBtn2.Content.ToString();
-//                }
-//                else
-//                {
-//                    if (RadioBtn3.IsChecked == true)
-//                    {
-//                        response = RadioBtn3.Content.ToString();
-//                    }
-//                    else
-//                    {
-//                        if (RadioBtn4.IsChecked == true)
-//                        {
-//                            response = RadioBtn4.Content.ToString();
-//                        }
-//                        else
-//                        {
-//                            response = null;
-//                        }
-//                    }
-//                }
-//            }
+            if (RadioBtn1.IsChecked == true)
+            {
+                response = RadioBtn1.Content.ToString();
+            }
+            else
+            {
+                if (RadioBtn2.IsChecked == true)
+                {
+                    response = RadioBtn2.Content.ToString();
+                }
+                else
+                {
+                    if (RadioBtn3.IsChecked == true)
+                    {
+                        response = RadioBtn3.Content.ToString();
+                    }
+                    else
+                    {
+                        if (RadioBtn4.IsChecked == true)
+                        {
+                            response = RadioBtn4.Content.ToString();
+                        }
+                        else
+                        {
+                            response = null;
+                        }
+                    }
+                }
+            }
 
-//            ActOnAnswerGivenGeekQuiz(response);
-//        }
+            ActOnAnswerGivenGeekQuiz(response);
+        }
 
-//        private void ActOnAnswerGivenGeekQuiz(string response)
-//        {
-//            GeekQuizReplyEmoticonCorrect.Visibility = Visibility.Collapsed;
-//            GeekQuizReplyEmoticonIncorrect.Visibility = Visibility.Collapsed;
+        private void ActOnAnswerGivenGeekQuiz(string response)
+        {
+            GeekQuizReplyEmoticonCorrect.Visibility = Visibility.Collapsed;
+            GeekQuizReplyEmoticonIncorrect.Visibility = Visibility.Collapsed;
 
-//            var isResponseCorrect = IsResponseCorrectGeekQuiz(response);
+            var isResponseCorrect = IsResponseCorrectGeekQuiz(response);
 
-//            if (isResponseCorrect)
-//            {
-//                TextBlockGeekQuizReply.Text = "Well,done - correct answer !";
-//                SetQuizReplyColourGeekQuiz(Colors.Green);
-//                AppBtnGeekQuizSubmitMultiChoiceAnwser.IsEnabled = false;
-//                GeekQuizReplyEmoticonCorrect.Visibility = Visibility.Visible;
+            if (isResponseCorrect)
+            {
+                TextBlockGeekQuizReply.Text = "Well,done - correct answer !";
+                SetQuizReplyColourGeekQuiz(Colors.Green);
+                AppBtnGeekQuizSubmitMultiChoiceAnwser.IsEnabled = false;
+                GeekQuizReplyEmoticonCorrect.Visibility = Visibility.Visible;
 
-//                if (!_userStatusTotalsIncremented && _totalQuestionsAnsweredCorrectly.HasValue)
-//                {
-//                    _totalQuestionsAnsweredCorrectly++;
-//                }
-//            }
-//            else
-//            {
-//                if (response == null)
-//                {
-//                    TextBlockGeekQuizReply.Text = "No cheating please - you must supply an answer.";
-//                    SetQuizReplyColourGeekQuiz(Colors.Orange);
-//                }
-//                else
-//                {
-//                    TextBlockGeekQuizReply.Text = "Oh dear - wrong answer.";
+                if (!_userStatusTotalsIncremented && _totalQuestionsAnsweredCorrectly.HasValue)
+                {
+                    _totalQuestionsAnsweredCorrectly++;
+                }
+            }
+            else
+            {
+                if (response == null)
+                {
+                    TextBlockGeekQuizReply.Text = "No cheating please - you must supply an answer.";
+                    SetQuizReplyColourGeekQuiz(Colors.Orange);
+                }
+                else
+                {
+                    TextBlockGeekQuizReply.Text = "Oh dear - wrong answer.";
 
-//                    if (_questionType == QuestionType.MultiChoice)
-//                    {
-//                        TextBlockGeekQuizReply.Text += $" The correct answer is {_correctAnswer}";
-//                    }
+                    if (_questionType == QuestionType.MultiChoice)
+                    {
+                        TextBlockGeekQuizReply.Text += $" The correct answer is {_correctAnswer}";
+                    }
 
-//                    AppBtnGeekQuizSubmitMultiChoiceAnwser.IsEnabled = false;
-//                    SetQuizReplyColourGeekQuiz(Colors.Red);
-//                    GeekQuizReplyEmoticonIncorrect.Visibility = Visibility.Visible;
-//                }
-//            }
+                    AppBtnGeekQuizSubmitMultiChoiceAnwser.IsEnabled = false;
+                    SetQuizReplyColourGeekQuiz(Colors.Red);
+                    GeekQuizReplyEmoticonIncorrect.Visibility = Visibility.Visible;
+                }
+            }
 
-//            TextBlockGeekQuizReply.Visibility = Visibility.Visible;
+            TextBlockGeekQuizReply.Visibility = Visibility.Visible;
 
-//            if (!_userStatusTotalsIncremented && _totalQuestionsAsked.HasValue)
-//            {
-//                _totalQuestionsAsked++;
-//                _userStatusTotalsIncremented = true;
-//            }
-//            var userStatus = GetUserStatusGeekQuiz(_totalQuestionsAnsweredCorrectly, _totalQuestionsAsked);
-//            AppTextBlockGeekQuizUserStatus.Text = userStatus;
+            if (!_userStatusTotalsIncremented && _totalQuestionsAsked.HasValue)
+            {
+                _totalQuestionsAsked++;
+                _userStatusTotalsIncremented = true;
+            }
+            var userStatus = GetUserStatusGeekQuiz(_totalQuestionsAnsweredCorrectly, _totalQuestionsAsked);
+            AppTextBlockGeekQuizUserStatus.Text = userStatus;
 
-//            PersistHiddenOptionsEventHandler?.Invoke(_totalQuestionsAsked, _totalQuestionsAnsweredCorrectly);
-//        }
+            PersistHiddenOptionsEventHandler?.Invoke(_totalQuestionsAsked, _totalQuestionsAnsweredCorrectly);
+        }
 
-//        internal string GetUserStatusGeekQuiz(int? totalQuestionsAnsweredCorrectly, int? totalQuestionsAsked)
-//        {
-//            int percentageSuccess;
+        internal string GetUserStatusGeekQuiz(int? totalQuestionsAnsweredCorrectly, int? totalQuestionsAsked)
+        {
+            int percentageSuccess;
 
-//            if (totalQuestionsAsked == 0 ||
-//                !totalQuestionsAnsweredCorrectly.HasValue ||
-//                !totalQuestionsAsked.HasValue)
-//            {
-//                percentageSuccess = 0;
-//            }
-//            else
-//            {
-//                //gregt unit test required
-//                double percentage = ((double)totalQuestionsAnsweredCorrectly.Value / totalQuestionsAsked.Value) * 100;
-//                percentageSuccess = (int)Math.Round(percentage, MidpointRounding.AwayFromZero);
-//            }
+            if (totalQuestionsAsked == 0 ||
+                !totalQuestionsAnsweredCorrectly.HasValue ||
+                !totalQuestionsAsked.HasValue)
+            {
+                percentageSuccess = 0;
+            }
+            else
+            {
+                //gregt unit test required
+                double percentage = ((double)totalQuestionsAnsweredCorrectly.Value / totalQuestionsAsked.Value) * 100;
+                percentageSuccess = (int)Math.Round(percentage, MidpointRounding.AwayFromZero);
+            }
 
-//            var userStatusDescription = percentageSuccess.UserStatusDescription();
+            var userStatusDescription = percentageSuccess.UserStatusDescription();
 
-//            var ranking = "Your rank: " + userStatusDescription;
+            var ranking = "Your rank: " + userStatusDescription;
 
-//            var successRate = percentageSuccess + "% success rate (" +
-//                              totalQuestionsAnsweredCorrectly + " out of " +
-//                              totalQuestionsAsked + ")";
+            var successRate = percentageSuccess + "% success rate (" +
+                              totalQuestionsAnsweredCorrectly + " out of " +
+                              totalQuestionsAsked + ")";
 
-//            var userStatus = ranking + " " + successRate;
+            var userStatus = ranking + " " + successRate;
 
-//            return userStatus;
-//        }
+            return userStatus;
+        }
 
-//        private void SetQuizReplyColourGeekQuiz(Color color)
-//        {
-//            TextBlockGeekQuizReply.Foreground = new SolidColorBrush(color);
-//        }
+        private void SetQuizReplyColourGeekQuiz(Color color)
+        {
+            TextBlockGeekQuizReply.Foreground = new SolidColorBrush(color);
+        }
 
-//        private bool IsResponseCorrectGeekQuiz(string response)
-//        {
-//            var rightAnswer = response == _correctAnswer;
+        private bool IsResponseCorrectGeekQuiz(string response)
+        {
+            var rightAnswer = response == _correctAnswer;
 
-//            return rightAnswer;
-//        }
-//        #endregion
-//    }
-//}
+            return rightAnswer;
+        }
+        #endregion
+    }
+}
